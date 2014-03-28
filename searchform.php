922b70895f5e74e8afe3e59ba72f291579f4db36
Joy Kim <jojo080889@gmail.com>
2014-03-28 13:50:20 -0700

    style searchbox

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <?php if (comicpress_check_child_file('searchform') == false) { ?>
 <form method="get" id="searchform" action="<?php bloginfo('wpurl'); ?>/">
 	<div>
-		<label id="search_label" for="s-search">Search...</label>
-		<input type="text" name="s" id="s-search" onfocus="document.getElementById('search_label').style.display = 'none'" onblur="if (this.value == '') { document.getElementById('search_label').style.display = 'inline'; }" />
-		<button type="submit">&raquo;</button>
+		<label id="search_label" for="s-search"><i class="fa fa-search"></i></label>
+		<input type="text" name="s" id="s-search" placeholder="Search" />
+		<button type="submit"></button>
 	</div>
 	<div class="clear"></div>
 </form>
